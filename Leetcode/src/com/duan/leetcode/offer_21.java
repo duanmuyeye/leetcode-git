909d646f876117b23d412bd052a28bb01619df4a
duanmu <952911455@qq.com>
2022-07-20 20:03:34 +0800

    20220720_剑指Offer21 首尾指针与快慢指针

--- Leetcode/src/com/duan/leetcode/offer_21.java
+++ Leetcode/src/com/duan/leetcode/offer_21.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 public class offer_21 {
 
     public int[] exchange(int[] nums) {
+        //傻瓜解法
 //        int[] result=new int[nums.length];
 //        ArrayList<Integer> tempOdd=new ArrayList<Integer>();
 //        ArrayList<Integer> tempEven=new ArrayList<Integer>();
@@ -21,14 +22,32 @@ public class offer_21 {
 //            result[i+tempOdd.size()]=tempEven.get(i);
 //        }
 //        return result;
-        int i = 0, j = nums.length - 1, tmp;
-        while (i < j) {
-            while (i < j && (nums[i] & 1) == 1) i++;
-            while (i < j && (nums[j] & 1) == 0) j--;
-            tmp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = tmp;
+
+        //首尾指针
+//        int i = 0, j = nums.length - 1, tmp;
+//        while (i < j) {
+//            while (i < j && (nums[i] & 1) == 1) i++;
+//            while (i < j && (nums[j] & 1) == 0) j--;
+//            tmp = nums[i];
+//            nums[i] = nums[j];
+//            nums[j] = tmp;
+//        }
+//        return nums;
+
+        //快慢指针
+        int i=0,j=0,tmp;
+        while(j<nums.length){
+            while(j<nums.length&&(nums[j]&1)==1){
+                tmp= nums[j];
+                nums[j]=nums[i];
+                nums[i]=tmp;
+                j++;
+                i++;
+            }
+            j++;
         }
         return nums;
+
+
     }
 }
