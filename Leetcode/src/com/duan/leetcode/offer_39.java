7b75d0cfed26136de37bb63d8bec451f3efe9cab
duanmu <952911455@qq.com>
2022-07-22 21:46:52 +0800

    20220722_剑指Offer39 数组出现次数超过一半的数字

---
+++ Leetcode/src/com/duan/leetcode/offer_39.java
@@ -0,0 +1,29 @@
+package com.duan.leetcode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+/*
+数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。
+你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+ */
+
+public class offer_39 {
+    //自己写的很垃圾的方法 太慢了 哈希表统计
+    public int majorityElement(int[] nums) {
+        double bounds = (nums.length / 2.0);
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int num:nums){
+            if(map.containsKey(num))map.put(num,map.get(num)+1);
+            else map.put(num,1);
+        }
+        for(int key:map.keySet()){
+            if(map.get(key)>bounds){
+                return key;
+            }
+        }
+        return -1;
+    }
+}
+
+
