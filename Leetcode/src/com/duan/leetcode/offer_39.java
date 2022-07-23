cc8264fa29189d00f99da00ba453a114e566594e
duanmu <952911455@qq.com>
2022-07-23 11:32:03 +0800

    20220722_剑指Offer39 数组出现次数超过一半的数字 hashmap和摩尔投票法

--- Leetcode/src/com/duan/leetcode/offer_39.java
+++ Leetcode/src/com/duan/leetcode/offer_39.java
@@ -10,19 +10,31 @@ import java.util.HashSet;
 
 public class offer_39 {
     //自己写的很垃圾的方法 太慢了 哈希表统计
+//    public int majorityElement(int[] nums) {
+//        double bounds = (nums.length / 2.0);
+//        HashMap<Integer, Integer> map = new HashMap<>();
+//        for (int num : nums) {
+//            if (map.containsKey(num)) map.put(num, map.get(num) + 1);
+//            else map.put(num, 1);
+//        }
+//        for (int key : map.keySet()) {
+//            if (map.get(key) > bounds) {
+//                return key;
+//            }
+//        }
+//        return -1;
+//    }
+
+
+    //摩尔投票法  可以投出超过一半的数字
     public int majorityElement(int[] nums) {
-        double bounds = (nums.length / 2.0);
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int num:nums){
-            if(map.containsKey(num))map.put(num,map.get(num)+1);
-            else map.put(num,1);
+        int votes = 0, x = 0;
+        for (int num : nums) {
+            if (votes == 0)
+                x = num;
+            votes += num == x ? 1 : -1;
         }
-        for(int key:map.keySet()){
-            if(map.get(key)>bounds){
-                return key;
-            }
-        }
-        return -1;
+        return x;
     }
 }
 
